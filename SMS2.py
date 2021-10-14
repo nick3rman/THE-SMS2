e0ffab226c5a18087c067916160821effe575880
UNITY <74477764+UNITY484@users.noreply.github.com>
2021-10-14 23:55:45 +0700

    Add files via upload

---
+++ SMS2.py
@@ -0,0 +1,12 @@
+# Python program to print
+# red text with green background
+ 
+from colorama import Fore, Back, Style
+import time 
+import os
+os.system("apt update")
+os.system("clear")
+print(Fore.GREEN +"Error SMS2.py")
+print("")
+time.sleep(3)
+os.system("yes Error")
